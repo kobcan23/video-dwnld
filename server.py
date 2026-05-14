325163485ba6a04c6fc964877d4437cced39e720
kobcan23 <koburneev@gmail.com>
2026-05-14 14:36:15 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def do_download(task_id, url, password):
     out_dir.mkdir(exist_ok=True)
     ydl_opts = {
         'outtmpl': str(out_dir / '%(title)s.%(ext)s'),
-        'format': 'bestvideo+bestaudio/best/bestvideo/bestaudio',
+        'format': 'best[ext=mp4]/best',
         'merge_output_format': 'mp4',
         'quiet': True,
         'no_warnings': True,
